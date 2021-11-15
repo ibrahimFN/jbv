789a9800adb7ecc021fbe29d94807fe964fb6587
JMTHON <85131843+JMTHON-AR@users.noreply.github.com>
2021-11-16 01:05:51 +0300

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -41,34 +41,32 @@ except Exception as e:
     sys.exit()
 
 
-class JmtCheck:
+class Catcheck:
     def __init__(self):
         self.sucess = True
 
 
-JmtCheck = JmtCheck()
+Catcheck = Catcheck()
 
 
 async def startup_process():
     check = await ipchange()
     if check is not None:
-        JmtCheck.sucess = False
+        Catcheck.sucess = False
         return
     await verifyLoggerGroup()
     await load_plugins("plugins")
     await load_plugins("assistant")
     print("--------------------------------------------------------")
     print("تم بنجاح اكتمال تنصيب سورس جمثون المجاني ✓")
-    print(
-        " - ارسل  فحص  للتأكد من البوت\n-  ولعرض اوامر السورس ارشل  .الاوامر\n-  للمزيد من المعلومات ادخل الى مجموعتك في التليجرام"
-    )
+    print(" - ارسل  فحص  للتأكد من البوت\n-  ولعرض اوامر السورس ارشل  .الاوامر\n-  للمزيد من المعلومات ادخل الى مجموعتك في التليجرام")
     print("-------------------------------------------------------")
     await verifyLoggerGroup()
     await add_bot_to_logger_group(BOTLOG_CHATID)
     if PM_LOGGER_GROUP_ID != -100:
         await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
     await startupmessage()
-    JmtCheck.sucess = True
+    Catcheck.sucess = True
     return
 
 
@@ -77,7 +75,7 @@ jmthon.loop.run_until_complete(startup_process())
 
 if len(sys.argv) not in (1, 3, 4):
     jmthon.disconnect()
-elif not JmtCheck.sucess:
+elif not Catcheck.sucess:
     if HEROKU_APP is not None:
         HEROKU_APP.restart()
 else:
