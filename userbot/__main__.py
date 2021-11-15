b3958855711fe3171b0b5fcddd10be7f8575382a
JMTHON <85131843+JMTHON-AR@users.noreply.github.com>
2021-11-16 01:03:32 +0300

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,3 +1,12 @@
+# @Jmthon - < https://t.me/Jmthon >
+# Copyright (C) 2021 - JMTHON-AR
+# All rights reserved.
+#
+# This file is a part of < https://github.com/JMTHON-AR/JMTHON >
+# Please read the GNU Affero General Public License in;
+# < https://www.github.com/JMTHON-AR/LICENSE/blob/LICENSE/LICENSE/ 
+# ================================================================
+
 import sys
 
 import userbot
@@ -8,8 +17,6 @@ from .core.logger import logging
 from .core.session import jmthon
 from .utils import (
     add_bot_to_logger_group,
-    autojo,
-    autozs,
     ipchange,
     load_plugins,
     setup_bot,
@@ -17,62 +24,57 @@ from .utils import (
     verifyLoggerGroup,
 )
 
-LOGS = logging.getLogger("JMTHON")
+LOGS = logging.getLogger("جمثون")
 
 print(userbot.__copyright__)
-print("Licensed under the terms of the " + userbot.__license__)
+print("جميع الحقوق والملفات محفوظة " + userbot.__license__)
 
 cmdhr = Config.COMMAND_HAND_LER
 
 try:
-    LOGS.info("Starting Userbot")
+    LOGS.info(f"⚒️ يتم تشغيل جمثون")
     jmthon.loop.run_until_complete(setup_bot())
-    LOGS.info("TG Bot Startup Completed")
+    LOGS.info(f"✅ انتهاء التشغيل ")
+ 
 except Exception as e:
     LOGS.error(f"{str(e)}")
     sys.exit()
 
 
-class CatCheck:
+class JmtCheck:
     def __init__(self):
         self.sucess = True
 
 
-Catcheck = CatCheck()
+JmtCheck = JmtCheck()
 
 
 async def startup_process():
     check = await ipchange()
     if check is not None:
-        Catcheck.sucess = False
+        JmtCheck.sucess = False
         return
     await verifyLoggerGroup()
     await load_plugins("plugins")
     await load_plugins("assistant")
-    print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
-    print("⌯︙بـوت جـمثون يعـمل بـنجاح ")
-    print(
-        f"يجـب تفـعيل وضع الأنلايـن ثم أرسـل {cmdhr}فحص لـرؤيـة اذا كـان البوت شـغال\
-        \nللمسـاعدة تواصـل  https://t.me/jmthon"
-    )
-    print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+    print("--------------------------------------------------------")
+    print("تم بنجاح اكتمال تنصيب سورس جمثون المجاني ✓")
+    print(" - ارسل  فحص  للتأكد من البوت\n-  ولعرض اوامر السورس ارشل  .الاوامر\n-  للمزيد من المعلومات ادخل الى مجموعتك في التليجرام")
+    print("-------------------------------------------------------")
     await verifyLoggerGroup()
     await add_bot_to_logger_group(BOTLOG_CHATID)
     if PM_LOGGER_GROUP_ID != -100:
         await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
     await startupmessage()
-    Catcheck.sucess = True
+    JmtCheck.sucess = True
     return
 
-
 jmthon.loop.run_until_complete(startup_process())
-jmthon.loop.run_until_complete(autozs())
-jmthon.loop.run_until_complete(autojo())
 
 
 if len(sys.argv) not in (1, 3, 4):
     jmthon.disconnect()
-elif not Catcheck.sucess:
+elif not JmtCheck.sucess:
     if HEROKU_APP is not None:
         HEROKU_APP.restart()
 else:
